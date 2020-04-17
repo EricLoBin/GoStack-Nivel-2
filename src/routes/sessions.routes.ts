e7a0a30eee9e20cc9fedd3230e3a20a7fabd7f96
EricLoBin <ekilobin@gmail.com>
2020-04-17 11:10:31 -0300

    [ADD/UPDATE] Validando credenciais

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import AuthentivateUserService from '../services/AuthentivateUserService';
+
+const sessionsRouter = Router();
+
+// POST http://localhost:3333/sessions
+
+sessionsRouter.post('/', async (request, response) => {
+    try {
+        const { email, password } = request.body;
+
+        const authentivateUser = new AuthentivateUserService();
+
+        const { user } = await authentivateUser.execute({
+            email,
+            password,
+        });
+
+        delete user.password;
+
+        return response.json({ user });
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default sessionsRouter;
